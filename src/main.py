1661427eaafb267619ccb13c510d2c33e9ab2a79
Karen Mejia <karen.mejia@udea.edu.co>
2024-12-11 04:24:22 -0500

    Se activa el main como disparador del proceso

--- src/main.py
+++ src/main.py
@@ -13,45 +13,48 @@ from utils.calculations import calculations
 
 pd.set_option('display.max_rows', None)
 
-# Definicion de rutas
-main_path = Constants.main_path.value
-json_path = os.path.join(main_path, 'json')
-report_path = os.path.join(main_path,'profiling/profiling_report')
-parquet_path = os.path.join(main_path,'data')
-db_path = os.path.join(main_path,'db')
+def main():
+    # Definicion de rutas
+    main_path = Constants.main_path.value
+    json_path = os.path.join(main_path, 'json')
+    report_path = os.path.join(main_path,'profiling/profiling_report')
+    parquet_path = os.path.join(main_path,'data')
+    db_path = os.path.join(main_path,'db')
 
-year = Constants.year.value
-month = Constants.month.value
+    year = Constants.year.value
+    month = Constants.month.value
 
-# Creacion de una instancia de la clase APIExtraction
-api = APIExtraction(base_url="http://api.tvmaze.com/schedule/web", save_directory=json_path)
+    # Creacion de una instancia de la clase APIExtraction
+    api = APIExtraction(base_url="http://api.tvmaze.com/schedule/web", save_directory=json_path)
 
-# Creacion del generador
-date_generator = iteration(year,month)
-last_day = calendar.monthrange(year, month)[1]
+    # Creacion del generador
+    date_generator = iteration(year,month)
+    last_day = calendar.monthrange(year, month)[1]
 
-for day in range(0,last_day):
-    current_date = next(date_generator)
-    datos = api.fetch_data(current_date)
-    json_file = 'series_'+str(current_date)+'.json'
-    api.save_json(datos, json_file)
+    for day in range(0,last_day):
+        current_date = next(date_generator)
+        datos = api.fetch_data(current_date)
+        json_file = 'series_'+str(current_date)+'.json'
+        api.save_json(datos, json_file)
 
-df = normalize(json_path)
-data_profiling(df,report_path)
-cleaned_df = cleaning(df, "_embedded.show.schedule.days", "_embedded.show.genres")
-df_parquet = to_parquet(cleaned_df, parquet_path)
-df_parquet = pd.read_parquet(parquet_path)
-df_parquet = df_parquet.astype("object")
-df_parquet = df_parquet.where(pd.notnull(df_parquet), "None")
+    df = normalize(json_path)
+    data_profiling(df,report_path)
+    cleaned_df = cleaning(df, "_embedded.show.schedule.days", "_embedded.show.genres")
+    df_parquet = to_parquet(cleaned_df, parquet_path)
+    df_parquet = pd.read_parquet(parquet_path)
+    df_parquet = df_parquet.astype("object")
+    df_parquet = df_parquet.where(pd.notnull(df_parquet), "None")
 
-Helper = Helper(df_parquet, db_path)
-Helper.create_table(Constants.table_name.value, Constants.fields.value)
-Helper.data_insert(Constants.table_name.value, Constants.fields_insert.value)
+    Helper = Helper(df_parquet, db_path)
+    Helper.create_table(Constants.table_name.value, Constants.fields.value)
+    Helper.data_insert(Constants.table_name.value, Constants.fields_insert.value)
 
-avg_runtime, genre_counts, unique_domains = calculations(df_parquet)
-print("INFORMACION SOBRE SHOWS DEL MES:\n")
-print("Runtime promedio (averageRuntime):" + str(avg_runtime))
-print('\n')
-print("Conteo de shows de tv por género:\n", genre_counts)
-print('\n')
-print("Listado dominios únicos (web) del sitio oficial de los shows:\n", unique_domains)
+    avg_runtime, genre_counts, unique_domains = calculations(df_parquet)
+    print("INFORMACION SOBRE SHOWS DEL MES:\n")
+    print("Runtime promedio (averageRuntime):" + str(avg_runtime))
+    print('\n')
+    print("Conteo de shows de tv por género:\n", genre_counts)
+    print('\n')
+    print("Listado dominios únicos (web) del sitio oficial de los shows:\n", unique_domains)
+
+main()
