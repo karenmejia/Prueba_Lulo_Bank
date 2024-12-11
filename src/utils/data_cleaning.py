d51d8c669f818769754bbe6976bccca1984aebcc
Karen Mejia <karen.mejia@udea.edu.co>
2024-12-11 03:41:57 -0500

    Se crea base de datos sqlite y se realizan operaciones de agregacion

--- src/utils/data_cleaning.py
+++ src/utils/data_cleaning.py
@@ -1,5 +1,8 @@
+import pandas as pd
+
 def cleaning(df, *column_name):
-    df[column_name[0]] = df[column_name[0]].apply(lambda x: "None" if x == "[]" else x)
-    df[column_name[1]] = df[column_name[1]].apply(lambda x: "None" if x == "[]" else x)
+    df[column_name[0]] = df[column_name[0]].apply(lambda x: str(x))
+    df[column_name[1]] = df[column_name[1]].apply(lambda x: str(x))
+    df = df.applymap(lambda x: x.replace("'", "") if isinstance(x, str) else x)
     df.columns = df.columns.str.replace('.', '_')
     return df
